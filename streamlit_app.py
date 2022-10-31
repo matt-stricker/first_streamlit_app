4f6195e63dd90c27717a860d412d1a727b0b190e
mirkwood11 <88983681+mirkwood11@users.noreply.github.com>
2022-10-31 11:59:25 -1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 Oatmeal')
+streamlit.text('Kale-Spin Smoothie')
+streamlit.text('Free Range Egg')
