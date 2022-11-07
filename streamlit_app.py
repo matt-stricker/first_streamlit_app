da1c4175c921c169f53f874fd890fc508a39d745
mirkwood11 <88983681+mirkwood11@users.noreply.github.com>
2022-11-07 08:56:24 -1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 Oatmeal')
-streamlit.text('Kale-Spin Smoothie')
-streamlit.text('Free Range Egg')
+streamlit.text('🥣 Omega 3 Oatmeal')
+streamlit.text('🥗 Kale-Spin Smoothie')
+streamlit.text('🐔 Free Range Egg')
+streamlit.text('🥑🍞 Avacado Toast!')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
